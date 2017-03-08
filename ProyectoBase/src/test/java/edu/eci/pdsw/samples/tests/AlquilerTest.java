8d388fa2a354cb4146ace85a3b9b9e6506e4a1d3
CarolMeneses <carol.meneses@mail.escuelaing.edu.co>
2017-03-08 16:18:18 -0500

    Modificado AlquilerTest

--- ProyectoBase/src/test/java/edu/eci/pdsw/samples/tests/AlquilerTest.java
+++ ProyectoBase/src/test/java/edu/eci/pdsw/samples/tests/AlquilerTest.java
@@ -7,9 +7,12 @@ package edu.eci.pdsw.samples.tests;
 
 import edu.eci.pdsw.samples.entities.Cliente;
 import edu.eci.pdsw.samples.entities.Item;
+import edu.eci.pdsw.samples.entities.ItemRentado;
 import edu.eci.pdsw.samples.services.ExcepcionServiciosAlquiler;
 import edu.eci.pdsw.samples.services.ServiciosAlquiler;
 import edu.eci.pdsw.samples.services.ServiciosAlquilerItemsStub;
+import java.sql.Date;
+import java.util.List;
 import org.junit.Before;
 import org.junit.Test;
 import static org.junit.Assert.*;
@@ -39,6 +42,9 @@ public class AlquilerTest {
     public void setUp() {
     }
     
+    /**
+    *No debería cobrar multa cuando se entrega la pelicula en el día limite
+    */
     @Test
     public void CF1Test() throws ExcepcionServiciosAlquiler{
         ServiciosAlquiler sa=ServiciosAlquilerItemsStub.getInstance();
@@ -58,7 +64,9 @@ public class AlquilerTest {
                 
     }
     
-
+    /**
+    *Debería cobrar la multa por 3 días de trabajo
+    */
     @Test
     public void CE1Test() throws ExcepcionServiciosAlquiler{
         ServiciosAlquiler sa=ServiciosAlquilerItemsStub.getInstance();
@@ -76,10 +84,37 @@ public class AlquilerTest {
                 ,sa.valorMultaRetrasoxDia()*3,sa.consultarMultaAlquiler(55, java.sql.Date.valueOf("2005-12-28")));
                 
     }
-    
-    
-    
-    
-    
+    /**
+     * La fecha de devolución debe ser mayor o igual a la fecha de alquiler
+     */
+    @Test
+    public void CE2Test() throws ExcepcionServiciosAlquiler{
+        ServiciosAlquiler sa=ServiciosAlquilerItemsStub.getInstance();
+                                
+        Item i1=new Item(sa.consultarTipoItem(1), 55, "Los 5 Fantasticos", "Los 5 Fantásticos  es una película de superhéroes  basada en la serie de cómic homónima de Marvel.", java.sql.Date.valueOf("2005-06-08"), 2000, "DVD", "Ciencia Ficcion");        
+        sa.registrarCliente(new Cliente("Juanito Perez",9269,"24234","calle 123","aaa@gmail.com"));
+        sa.registrarItem(i1);
+                
+        Item item=sa.consultarItem(55);
+        
+        sa.registrarAlquilerCliente(java.sql.Date.valueOf("2005-12-20"), 9269, item, 5);
+        List<ItemRentado> ir= null;
+        ir=sa.consultarItemsCliente(9843);
+        Date devolucion=java.sql.Date.valueOf("2005-12-22");
+        int fecha=ir.get(0).getFechainiciorenta().compareTo(devolucion); //
+        assertTrue(fecha<0);
+    }
+
+    /**
+     * El costo del alquiler no puede ser mayor al costo de multa diaria
+     */
+     /**
+     * El costo del alquiler no puede ser mayor al costo de multa diaria
+     */
+     /**
+     * La tarifa de alquiler debe ser mayor a 0
+     */ /**
+     * Debe permitir modificar la tarifa
+     */
     
 }
