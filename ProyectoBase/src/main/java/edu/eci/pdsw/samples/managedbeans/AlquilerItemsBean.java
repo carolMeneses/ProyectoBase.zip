a8a5b1f3428579b7a92a4ec7709d16bcfaa2777c
MariaPaulaP <maria.pinzon-c@mail.escuelaing.edu.co>
2017-03-09 11:16:49 -0500

    correccion been

--- ProyectoBase/src/main/java/edu/eci/pdsw/samples/managedbeans/AlquilerItemsBean.java
+++ ProyectoBase/src/main/java/edu/eci/pdsw/samples/managedbeans/AlquilerItemsBean.java
@@ -5,8 +5,12 @@
  */
 package edu.eci.pdsw.samples.managedbeans;
 
+import edu.eci.pdsw.samples.entities.Cliente;
+import edu.eci.pdsw.samples.entities.ItemRentado;
 import edu.eci.pdsw.samples.services.ServiciosAlquiler;
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
 
@@ -18,9 +22,83 @@ import javax.faces.bean.SessionScoped;
 @SessionScoped
 public class AlquilerItemsBean implements Serializable {
 
+    String nombre;
+    long documento;
+    String telefono;
+    String direccion;
+    String email;
+
+    Cliente ClienteNuevo;
+    ArrayList<Cliente> acumulados = new ArrayList<>();
+
     ServiciosAlquiler sp = ServiciosAlquiler.getInstance();
 
     public AlquilerItemsBean() {
+        nombre = "";
+        documento = 0;
+        telefono = "";
+        direccion = "";
+        email = "";
+        ClienteNuevo = new Cliente(nombre, documento, telefono, direccion, email);
+
+    }
+
+    public void adicionarCliente() {
+        ClienteNuevo = new Cliente(nombre, documento, telefono, direccion, email);
+        acumulados.add(ClienteNuevo);
+        System.out.println(getNombre());
+    }
+
+    public String getNombre() {
+        return ClienteNuevo.getNombre();
+    }
+
+    public void setNomb(String nomb) {
+        this.nombre = nomb;
+    }
+
+    public long getDocumento() {
+        return ClienteNuevo.getDocumento();
+    }
+
+    public void setDocumento(long documento) {
+        this.documento = documento;
+    }
+
+    public String getTelefono() {
+        return ClienteNuevo.getTelefono();
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public String getDireccion() {
+        return ClienteNuevo.getDireccion();
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    public String getEmail() {
+        return ClienteNuevo.getEmail();
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public boolean isVetado() {
+        return ClienteNuevo.isVetado();
+    }
+
+    public ArrayList<ItemRentado> getRentados() {
+        return ClienteNuevo.getRentados();
+    }
+
+    public List<Cliente> getAcumulados() {
+        return acumulados;
     }
 
 }
